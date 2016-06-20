95dc72bf03617143b4822b9743843d9ae3add9f7
Amer Justice <amerjustice@outlook.com>
2016-06-20 11:23:33 -0700

    Robot Maze
    robot seeks a star on it’s own

---
+++ Maze/ControlCenter.swift
@@ -0,0 +1,52 @@
+//
+//  ControlCenter.swift
+//  Maze
+//
+//  Created by Jarrod Parkes on 8/14/15.
+//  Copyright © 2015 Udacity, Inc. All rights reserved.
+//
+import UIKit
+
+class ControlCenter {
+
+    var mazeController: MazeController!
+
+    func moveComplexRobot(myRobot: ComplexRobotObject) {
+        
+      
+        let robotIsBlocked = isFacingWall(myRobot, direction: myRobot.direction)
+        let myWallInfo = checkWalls(myRobot)
+        let isThreeWayJunction = (myWallInfo.numberOfWalls == 1)
+        let isTwoWayPath = (myWallInfo.numberOfWalls == 2)
+        let isDeadEnd = (myWallInfo.numberOfWalls == 3)
+        
+        
+
+        if isThreeWayJunction && robotIsBlocked {
+            randomlyRotateRightOrLeft(myRobot)
+        } else if isThreeWayJunction && !robotIsBlocked {
+            continueStraightOrRotate(myRobot, wallInfo: myWallInfo)
+           
+        } else if isTwoWayPath && !robotIsBlocked {
+            myRobot.move()
+           
+        } else if isTwoWayPath && robotIsBlocked {
+            turnTowardClearPath(myRobot, wallInfo: myWallInfo)
+        }
+         
+        else if isDeadEnd {
+            
+            if isDeadEnd && !robotIsBlocked {
+                myRobot.move()
+            }
+            
+            if isDeadEnd && robotIsBlocked {
+                myRobot.rotateRight()
+            }
+        }
+    }
+    
+    func previousMoveIsFinished(robot: ComplexRobotObject) {
+        self.moveComplexRobot(robot)
+    }
+}
---
+++ Maze/WallLocations.swift
@@ -0,0 +1,75 @@
+//
+//  WallLocations.swift
+//  Maze
+//
+//  Created by Gabrielle Miller-Messner on 12/1/15.
+//  Copyright © 2015 Udacity, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension ControlCenter {
+    
+    func isFacingWall(robot: ComplexRobotObject, direction: MazeDirection) -> Bool {
+        
+        let cell = mazeController.currentCell(robot)
+        var isWall: Bool = false
+        
+
+        switch(direction) {
+        case .Up:
+            if cell.top {
+                isWall = true
+            }
+        case .Down:
+            if cell.bottom {
+                isWall = true
+            }
+        case .Left:
+            if cell.left {
+                isWall = true
+            }
+        case .Right:
+            if cell.right {
+                isWall = true
+            }
+        }
+        
+    
+        
+        return isWall
+    }
+    
+    func checkWalls(robot:ComplexRobotObject) -> (up: Bool, right: Bool, down: Bool, left: Bool, numberOfWalls: Int) {
+        var numberOfWalls = 0
+        let cell = mazeController.currentCell(robot)
+        
+        
+        let isWallUp = cell.top
+        if isWallUp {
+            numberOfWalls++
+        }
+        
+      
+        let isWallRight = cell.right
+        if isWallRight {
+            numberOfWalls++
+        }
+        
+  
+        let isWallBottom = cell.bottom
+        if isWallBottom {
+            numberOfWalls++
+        }
+        
+ 
+        let isWallLeft = cell.left
+        if isWallLeft {
+            numberOfWalls++
+        }
+        
+   
+        return (isWallUp, isWallRight, isWallBottom, isWallLeft, numberOfWalls)
+    }
+    
+}
